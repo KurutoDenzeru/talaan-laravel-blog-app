875a380762898febd409e15b6c5f970f7bf4832a
Kuruto Denzeru <kurtdenzel51@gmail.com>
2023-11-27 23:54:58 +0800

    Create logo.jsx

---
+++ resources/js/Components/logo.jsx
@@ -0,0 +1,14 @@
+import GDSCLOGO from 'public/images/';
+
+function GDSCUECLogo() {
+    return (
+        // <img src={GDSCLOGO} alt="Example" />
+        <p>hello</p>
+    );
+}  
+
+export default function GDSCSVGLOGO() {
+    return (
+        <GDSCUECLogo />
+    );
+    }
