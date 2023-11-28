6b219b24867ab6263dce57c17439a596276bc148
Kurt Carvey Cadenas <kurtcarveycadenas2001@gmail.com>
2023-11-28 12:22:04 +0800

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,15 +1,15 @@
 import { Link, Head } from '@inertiajs/react';
 import Header from '../Components/Landing/header.jsx'
 import HeroLogin from '../Components/Landing/herologin.jsx';
-import UniversalFooter from '../Components/footer.jsx';
+// import UniversalFooter from '../Components/footer.jsx';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Index" />
-            
+
             <HeroLogin />
-            <UniversalFooter />
+            {/*<UniversalFooter />*/}
 
         </>
     );
