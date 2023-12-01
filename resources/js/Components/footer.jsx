8e310b8d66fc377d9b77ed5ecce4095b6fbaf455
Kuruto Denzeru <kurtdenzel51@gmail.com>
2023-12-01 23:32:28 +0800

    Create footer.jsx

---
+++ resources/js/Components/footer.jsx
@@ -0,0 +1,45 @@
+// import GDSCSVGLOGO from '../Components/logo.jsx';
+
+function HeroFooter() {
+    return (
+
+        <footer class="bg-gray-100 rounded-lg shadow">
+            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://gdsc-uec.vercel.app/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+
+                        {/* <img src={GDSCSVGLOGO} alt="Example" /> */}
+                        <img src="" class="h-8" alt="GDSC Logo" />
+
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap">GDSC UE Caloocan</span>
+                    </a>
+                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <span class="block text-sm text-gray-500 sm:text-center">© 2023 <a href="https://gdsc-uec.vercel.app/" class="hover:underline">Google Developers Students Club - UE Caloocan</a>. All Rights Reserved.</span>
+            </div>
+        </footer>
+
+    );
+}
+
+export default function UniversalFooter() {
+return (
+    <div>
+    <HeroFooter />
+    </div>
+);
+}
